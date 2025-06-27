8195bef1055cc8c287cffcc785d15036f389972b
elfe digital <elfesolution1@gmail.com>
2025-06-27 07:19:52 +0300

    Hheroo-reduced

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -44,10 +44,9 @@ const HeroSection = () => {
               <h4 className="text-base mb-4">HOTELS AND RESORTS</h4>
               <div className="mb-7 md:mb-8 lg:mb-9 xl:mb-10">
                 <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl 3xl:text-6xl font-semibold leading-[40px] md:leading-[50px] 3xl:leading-[70px]">
-                  THE BEST LUXURY HOTEL AND RESORT IN ETHIOPA 
+                  II IS A PLACE WHERE HAPPINESS HAPPENS 
                 </h1>
                 <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl 3xl:text-6xl font-semibold leading-[40px] lg:leading-[50px] 2xl:leading-[60px]">
-                  It is a place where happiness happens.
                 </h1>
               </div>
               <Link to={"/about"}>
@@ -69,88 +68,7 @@ const HeroSection = () => {
           </div>
         </SwiperSlide>
         {/* slider 2 */}
-
-        {/* slider 3 */}
-        <SwiperSlide>
-          <div
-            className="bg-[url('/images/home-1/Lewi-Resort-Wolaita-Sodo-Exterior-2.jpg')] w-full h-[700px] md:h-[800px] xl:h-[850px] 3xl:h-[950px]  bg-[rgba(30,30,30,0.4)] bg-opacity-40 grid items-center bg-cover justify-center text-white relative pb-[150px] lg:pb-20 xl:pb-0"
-            data-aos="fade-down"
-          >
-            <div className="font-Garamond 2xl:w-[720px] text-center">
-              <div className="flex space-x-2 items-center justify-center mb-5 lg:mb-6">
-                <FaStar className="w-[14px] h-[14px] lg:w-[16px] lg:h-[16px] text-khaki" />
-                <FaStar className="w-[14px] h-[14px] lg:w-[16px] lg:h-[16px] text-khaki" />
-                <FaStar className="w-[14px] h-[14px] lg:w-[16px] lg:h-[16px] text-khaki" />
-                <FaStar className="w-[14px] h-[14px] lg:w-[16px] lg:h-[16px] text-khaki" />
-                <FaStar className="w-[14px] h-[14px] lg:w-[16px] lg:h-[16px] text-khaki" />
-              </div>
-              <h4 className="text-base mb-4">LUXURY HOTEL AND RESORT</h4>
-              <div className="mb-7 md:mb-8 lg:mb-9 xl:mb-10">
-                <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl 3xl:text-6xl font-semibold leading-[40px] md:leading-[50px] 3xl:leading-[70px]">
-                  THE BEST LUXURY HOTEL
-                </h1>
-                <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl 3xl:text-6xl font-semibold leading-[40px] lg:leading-[50px] 2xl:leading-[60px]">
-                  IN COLOSSEUM
-                </h1>
-              </div>
-              <Link to={"/about"}>
-                <button
-                  className="w-[185px] h-[48px] lg:h-[56px] bg-khaki relative before:w-8 before:h-[1px] before:bg-khaki before:absolute before:left-0 before:top-16 
-                text-base
-               font-Garamond
-                font-medium mt-[-6px] hover-animBg after:bg-normalBlack after:rounded-none hover:before:bg-normalBlack uppercase"
-                >
-                  Discover More
-                </button>
-              </Link>
-            </div>
-            <div className="w-[221px] h-[50px] border-white border hidden md:flex items-center justify-center absolute left-0 top-1/2 -rotate-90">
-              <BiPhoneCall className="w-5 h-5 mr-2 text-khaki" /> +980 123 4567
-              890
-            </div>
-          </div>
-        </SwiperSlide>
-        {/* slider 4 */}
-        <SwiperSlide>
-          <div
-            className="bg-[url('/images/home-1/slider-img-301.jpg')] w-full h-[700px] md:h-[800px] xl:h-[850px] 3xl:h-[950px]  bg-[rgba(30,30,30,0.4)] bg-opacity-40 grid items-center bg-cover justify-center text-white relative pb-[150px] lg:pb-20 xl:pb-0"
-            data-aos="fade-down"
-          >
-            <div className="font-Garamond 2xl:w-[720px] text-center">
-              <div className="flex space-x-2 items-center justify-center mb-5 lg:mb-6">
-                <FaStar className="w-[14px] h-[14px] lg:w-[16px] lg:h-[16px] text-khaki" />
-                <FaStar className="w-[14px] h-[14px] lg:w-[16px] lg:h-[16px] text-khaki" />
-                <FaStar className="w-[14px] h-[14px] lg:w-[16px] lg:h-[16px] text-khaki" />
-                <FaStar className="w-[14px] h-[14px] lg:w-[16px] lg:h-[16px] text-khaki" />
-                <FaStar className="w-[14px] h-[14px] lg:w-[16px] lg:h-[16px] text-khaki" />
-              </div>
-              <h4 className="text-base mb-4">LUXURY HOTEL AND RESORT</h4>
-              <div className="mb-7 md:mb-8 lg:mb-9 xl:mb-10">
-                <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl 3xl:text-6xl font-semibold leading-[40px] md:leading-[50px] 3xl:leading-[70px]">
-                  THE BEST LUXURY HOTEL
-                </h1>
-                <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl 3xl:text-6xl font-semibold leading-[40px] lg:leading-[50px] 2xl:leading-[60px]">
-                  IN SRILANKA
-                </h1>
-              </div>
-              <Link to={"/about"}>
-                <button
-                  className="w-[185px] h-[48px] lg:h-[56px] bg-khaki relative before:w-8 before:h-[1px] before:bg-khaki before:absolute before:left-0 before:top-16 
-                text-base
-               font-Garamond
-                font-medium mt-[-6px] hover-animBg after:bg-normalBlack after:rounded-none hover:before:bg-normalBlack uppercase"
-                >
-                  Discover More
-                </button>
-              </Link>
-            </div>
-            <div className="w-[221px] h-[50px] border-white border hidden md:flex items-center justify-center absolute left-0 top-1/2 -rotate-90">
-              <BiPhoneCall className="w-5 h-5 mr-2 text-khaki" /> +251 930 10 83
-              59
-            </div>
-          </div>
-        </SwiperSlide>
-      </Swiper>
+        </Swiper>
     </div>
   );
 };
