6c4f2d4d4b503f76e76b1062dcbcc2adb6e32ec2
elfe digital <elfesolution1@gmail.com>
2025-06-27 06:47:14 +0300

    sample

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -29,10 +29,23 @@ const HeroSection = () => {
       >
         {/* slider 1 */}
         <SwiperSlide>
-          <div
-            className="bg-[url('/images/home-1/Lewishorttrailer.mp4')] w-full h-[700px] md:h-[800px] xl:h-[850px] 3xl:h-[950px]  bg-[#1e1e1ede] bg-opacity-90 grid items-center bg-cover justify-center text-white relative pb-[150px] lg:pb-16 xl:pb-0 "
-            data-aos="fade-down"
-          >
+          /// 
+        <div className="relative w-full h-[700px] md:h-[800px] xl:h-[850px] 3xl:h-[950px] grid items-center justify-center text-white pb-[150px] lg:pb-16 xl:pb-0 overflow-hidden">
+  <video
+    className="absolute top-0 left-0 w-full h-full object-cover"
+    autoPlay
+    muted
+    loop
+    playsInline
+  >
+    <source src="/images/home-1/Lewishorttrailer.mp4" type="video/mp4" />
+    Your browser does not support the video tag.
+  </video>
+  <div className="relative z-10 bg-[#1e1e1ede] bg-opacity-90 w-full h-full flex items-center justify-center">
+    {/* Your text/content goes here */}
+  </div>
+</div>
+            ///
             <div className="font-Garamond 2xl:w-[720px] text-center">
               <div className="flex space-x-2 items-center justify-center mb-5 lg:mb-6">
                 <FaStar className="w-[14px] h-[14px] lg:w-[16px] lg:h-[16px] text-[#1cc3b2]" />
